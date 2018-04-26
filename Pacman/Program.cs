5e40fd3733ea71bc81779dfe01cee7241cf79169
Vsevolod Shchur <sevaxxx.sx@gmail.com>
2018-04-26 12:43:28 +0500

    привет Ринат

--- Pacman/Program.cs
+++ Pacman/Program.cs
@@ -14,6 +14,7 @@ namespace Pacman
         [STAThread]
         static void Main()
         {
+            //привет Ринат
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new GameForm());
