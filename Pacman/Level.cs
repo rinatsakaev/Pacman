49a56eb05f6269e7ace79c186b485142a2bd2cd4
rinatsakaev <r.sakaevv@gmail.com>
2018-04-26 10:37:53 +0500

    test
    test

---
+++ Pacman/Level.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman
+{
+    class Level
+    {
+        public Level(string name, Point start, char[,] map)
+        {
+            Name = name;
+            Start = start;
+            Map = map;
+        }
+
+        public bool IsCompleted;
+        public readonly string Name;
+        public readonly Point Start;
+        public char[,] Map;
+        public Player Player;
+
+        public void MovePlayer(int deltaX, int deltaY)
+        {
+
+        }
+    }
+}
