fd5dddc0bab37a3f1771edfa0fd0a04271d9d69b
rinatsakaev <r.sakaevv@gmail.com>
2018-04-26 10:40:12 +0500

    trh
    rth

--- Pacman/GameForm.cs
+++ Pacman/GameForm.cs
@@ -1,12 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Pacman
 {
-    class GameForm
+    class GameForm : Form
     {
+        private Timer timer;
+        private Level currentLevel;
+        private Image player;
+        private Image ghost;
+        private Size spaceSize;
+        private bool right;
+        private bool left;
+        private bool up;
+        private bool down;
+        private void ChangeLevel(Level newLevel)
+        {
+
+        }
+
+        private void TimerTick(object sender, EventArgs e)
+        {
+            var controlX = left ? -1 : (right ? 1 : 0);
+            var controlY = down ? -1 : (up ? 1 : 0);
+            currentLevel.MovePlayer(controlX, controlY);
+
+        }
+        private void HandleKey(Keys e, bool isPressed)
+        {
+            if (e == Keys.A) left = isPressed;
+            if (e == Keys.D) right = isPressed;
+            if (e == Keys.W) up = isPressed;
+            if (e == Keys.S) down = isPressed;
+        }
     }
+ 
+
 }
--- Pacman/Player.cs
+++ Pacman/Player.cs
@@ -1,12 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Pacman
 {
-    class Player
+    class Player : ICreature
     {
+        public Point Corrdinates { get; set; }
+
+        public void Move(int deltaX, int deltaY)
+        {
+
+        }
+    }
+
+    class Ghost : ICreature
+    {
+        public Point Corrdinates { get; set; }
+
+        public void Move(int deltaX, int deltaY)
+        {
+
+        }
+    }
+
+    internal interface ICreature
+    {
+        Point Corrdinates { get; set; }
+        void Move(int deltaX, int deltaY);
     }
 }
