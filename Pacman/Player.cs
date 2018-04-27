74e8d3edc79ba9894995271b6d5fea6e0df72d4d
Vsevolod Shchur <sevaxxx.sx@gmail.com>
2018-04-27 14:07:06 +0500

    point sum 2

--- Pacman/Player.cs
+++ Pacman/Player.cs
@@ -3,44 +3,43 @@ using System.CodeDom;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Pacman
 {
-    public class PointExtension
+    public static class PointExtension
     {
-        public static Point Add(Point a, Point b)
+        public static Point Add(this Point a, int x, int y)
         {
-            return new Point(a.X + b.X, a.Y + b.Y);
+            return new Point(a.X + x, a.Y + y);
         }
 
     }
     class Player : ICreature
     {
-        public Point Corrdinates;
+        public Point Corrdinates { get; set; }
 
         public void Move(int deltaX, int deltaY)
         {
-
+            Corrdinates.Add(deltaX, deltaY);
         }
     }
 
     class Ghost : ICreature
     {
-        public Point Corrdinates;
+        public Point Corrdinates { get; set; }
 
         public void Move(int deltaX, int deltaY)
         {
-
+            Corrdinates.Add(deltaX, deltaY);
         }
     }
 
     internal interface ICreature
     {
-        Point Corrdinates;
+        Point Corrdinates { get; set; }
         void Move(int deltaX, int deltaY);
     }
 }
