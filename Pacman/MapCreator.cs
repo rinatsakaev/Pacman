0f8ecd279d0a5cc69c390620913601f452f2cbef
rinatsakaev <r.sakaevv@gmail.com>
2018-04-28 14:35:46 +0500

    Map creator

---
+++ Pacman/MapCreator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pacman
+{
+    class MapCreator
+    {
+        public static ICreature[,] CreateMap(string map, string separator = "\r\n")
+        {
+            var rows = map.Split(new[] { separator }, StringSplitOptions.RemoveEmptyEntries);
+            if (rows.Select(z => z.Length).Distinct().Count() != 1)
+                throw new Exception($"Wrong test map '{map}'");
+            var result = new ICreature[rows[0].Length, rows.Length];
+            for (var x = 0; x < rows[0].Length; x++)
+            for (var y = 0; y < rows.Length; y++)
+                result[x, y] = CreateCreatureBySymbol(rows[y][x]);
+            return result;
+        }
+
+        private static readonly Dictionary<string, Func<ICreature>> factory = new Dictionary<string, Func<ICreature>>();
+
+        private static ICreature CreateCreatureByTypeName(string name)
+        {
+            if (!factory.ContainsKey(name))
+            {
+                var type = Assembly
+                    .GetExecutingAssembly()
+                    .GetTypes()
+                    .FirstOrDefault(z => z.Name == name);
+                if (type == null)
+                    throw new Exception($"Can't find type '{name}'");
+                factory[name] = () => (ICreature)Activator.CreateInstance(type);
+            }
+            return factory[name]();
+        }
+
+
+        private static ICreature CreateCreatureBySymbol(char c)
+        {
+            switch (c)
+            {
+                case 'P':
+                    return CreateCreatureByTypeName("Player");
+                case 'T':
+                    return CreateCreatureByTypeName("Terrain");
+                case 'A':
+                    return CreateCreatureByTypeName("AngryGhost");
+                case 'F':
+                    return CreateCreatureByTypeName("FunkyGhost");
+                case 'I':
+                    return CreateCreatureByTypeName("InvisibleGhost");
+                case 'Z':
+                    return CreateCreatureByTypeName("Food");
+                case 'X':
+                    return CreateCreatureByTypeName("SuperFood");
+                case ' ':
+                    return null;
+                default:
+                    throw new Exception($"wrong character for ICreature {c}");
+            }
+        }
+    }
+}
